761fa72ca3d64487ad70e1f002ad82df2a7c5bd4
IsmailovaAisha <oyshaismailova08@gmail.com>
2025-09-19 20:30:35 +0500

    header

---
+++ main.js
@@ -0,0 +1,58 @@
+// let stuff=[true, "string", 22, {name:"Name"}]
+// stuff.forEach(value)((
+//     value.surname( "value")
+// ))
+
+let arr_number =[1, 2, 3, 4, 5, 6, 7, 8];
+let arr=[true, "string", 22, {name:"Name"}]
+let person_arr=[
+    {
+        id: 1,
+        name:"aisha",
+        surname:"Ismailove",
+        job:"business",
+        price:"10000000000"
+    },
+    {
+        id: 2,
+        name:"jamola",
+        surname:"rakhimova",
+        job:"doctor",
+        price:"100000000",
+    
+    },
+{
+    id: 3,
+    name:"alan",
+    surname:"asanov",
+    job:"therapist",
+    price:"1000000000",
+},    
+]
+
+// arr.forEach((value, idx, arr)=>{
+//     console.log(value, "value");
+//     console.log(idx, "idx");
+//     console.log(arr, "arr");
+// })
+
+// Array.prototype.myForEach = function (cb){
+//     for (let i=0; i<this.length; i++){
+//         cb(this[i], i, this);
+//     }
+// };
+
+// arr.myForEach((value, idx, arr)=> {
+//     console.log(value, idx, arr);
+// });
+
+// Array.prototype.myMap = function(cb){
+//     let result= [];
+// }
+// for (let i=0; i<this.length; i++){
+//         result.push(callback[i], i, this);
+//     };
+
+// arr.myMap((value, idx, arr)=> {
+//     console.log(value, idx, arr);
+// });
