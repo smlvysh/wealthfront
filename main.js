db60c9c7c73e1a051e3eefee1a442d9d678a5dcb
IsmailovaAisha <oyshaismailova08@gmail.com>
2025-09-20 17:41:01 +0500

    problems

--- main.js
+++ main.js
@@ -1,21 +1,33 @@
 let num= 10;
 let num2=20;
-let count;
-if(num2>num){
-    count=num2-num;
-}console.log(count);
+let count=0;
+for( let i=num; i<=num2; i++ ){
+    count+= i
+    console.log(count)
+}
 
 
-let arr = [6, -32, 87, -12, -76, 32, 866, -111]
-let pos=[0];
-let neg=[0];
-for(let i=0; i<=arr.length; i++){
-pos<arr;
-pos++
+// let arr = [6, -32, 87, -12, -76, 32, 866, -111]
+// let pos=[0];
+// let neg=[0];
+// for(let i=0; i<=arr.length; i++){
+// pos<arr;
+// pos++
+// }
+// for(let i=0; i>arr.length; i++){
+//     neg>arr;
+//     neg++
+// }
+// console.log(pos)
+// console.log(neg)
+
+function star(n){}
+for( let i=1; i<=n; i++){
+    let col= ""
+    for(let j= 1; j<=n; j++){
+        col+="*"
+        
 }
-for(let i=0; i>arr.length; i++){
-    neg>arr;
-    neg++
+console.log(col)
 }
-console.log(pos)
-console.log(neg)
+    star(5)
